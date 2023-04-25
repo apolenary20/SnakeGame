834bb13b6f731236d3e52abf8d90081636d20355
apolenary20 <71891178+apolenary20@users.noreply.github.com>
2023-04-25 14:17:57 +0300

    Загружена игра.
    Змейка может перемещаться и съедать бонусы

---
+++ Game.py
@@ -0,0 +1,82 @@
+import pygame
+import sys
+from Snake import Snake
+from random import randint
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.width, self.height = 640, 480
+        self.cell_size = 20
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Snake Game')
+        self.clock = pygame.time.Clock()
+
+        self.snake = Snake((self.height // 2 // self.cell_size, self.width // 4 // self.cell_size), (0, 1))
+        self.running = True
+        self.bonus = self.generate_bonus()
+
+    def generate_bonus(self):
+        while True:
+            bonus_position = (randint(0, self.height // self.cell_size - 1), randint(0, self.width // self.cell_size - 1))
+            if bonus_position not in self.snake.get_segments():
+                return bonus_position
+
+    def process_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN and self.snake.direction != (-1, 0):
+                    self.snake.change_direction((1, 0))
+                elif event.key == pygame.K_UP and self.snake.direction != (1, 0):
+                    self.snake.change_direction((-1, 0))
+                elif event.key == pygame.K_LEFT and self.snake.direction != (0, 1):
+                    self.snake.change_direction((0, -1))
+                elif event.key == pygame.K_RIGHT and self.snake.direction != (0, -1):
+                    self.snake.change_direction((0, 1))
+
+    def update(self):
+        self.snake.move()
+
+        if self.snake.get_head() == self.bonus:
+            self.snake.grow()
+            self.bonus = self.generate_bonus()
+
+        if self.check_collision():
+            self.running = False
+
+    def check_collision(self):
+        head = self.snake.get_head()
+        return (
+            head[0] < 0 or head[0] * self.cell_size >= self.height or
+            head[1] < 0 or head[1] * self.cell_size >= self.width or
+            self.snake.is_collision()
+        )
+
+    def render(self):
+        self.screen.fill((0, 0, 0))
+
+        # Отрисовка змейки
+        for segment in self.snake.get_segments():
+            pygame.draw.rect(self.screen, (255, 255, 255), (segment[1] * self.cell_size, segment[0] * self.cell_size, self.cell_size, self.cell_size))
+
+        # Отрисовка бонуса
+        pygame.draw.rect(self.screen, (255, 0, 0), (self.bonus[1] * self.cell_size, self.bonus[0] * self.cell_size, self.cell_size, self.cell_size))
+
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.process_input()
+            self.update()
+            self.render()
+            self.clock.tick(10)
+
+        pygame.quit()
+        sys.exit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
---
+++ Snake.py
@@ -0,0 +1,46 @@
+
+class Snake:
+    def __init__(self, position, direction):
+        self.segments = [position, (position[0] - direction[0], position[1] - direction[1])]
+        self.direction = direction
+        self.block_direction = False
+
+    def change_direction(self, new_direction):
+
+        if not self.block_direction:
+            if (new_direction[0], new_direction[1]) != (-self.direction[0], -self.direction[1]):
+                self.direction = new_direction
+
+        self.block_direction = True
+
+    def get_next_position(self):
+        self.block_direction = False
+        next_position = (self.segments[0][0] + self.direction[0], self.segments[0][1] + self.direction[1])
+        return next_position
+
+    def grow(self, position):
+        self.segments.insert(0, position)
+
+    def move(self):
+        next_position = self.get_next_position()
+        self.segments.pop()
+        self.segments.insert(0, next_position)
+
+    def is_collision(self):
+        return self.get_head() in self.segments[1:]
+
+    def get_head(self):
+        return self.segments[0]
+
+    def get_segments(self):
+        return self.segments
+
+    def grow(self):
+        last_segment = self.segments[-1]
+        second_last_segment = self.segments[-2]
+
+        delta_x = last_segment[1] - second_last_segment[1]
+        delta_y = last_segment[0] - second_last_segment[0]
+
+        new_segment = (last_segment[0] + delta_y, last_segment[1] + delta_x)
+        self.segments.append(new_segment)
